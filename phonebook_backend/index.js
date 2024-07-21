b90a9ca9f6ff4f57a2e96f8c9dd0e2547a3e0bec
Batfleck001 <harishsekar1423@gmail.com>
2024-07-21 22:02:12 +0530

    Done 3.2

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,9 +1,10 @@
 const express= require('express')
 const app = express()
+app.use(express.json())
 
-const persons = [
+let persons = [
     { 
-      id:"1",
+      id: "1",
       name: "Arto Hellas", 
       number: "040-123456"
     },
@@ -31,9 +32,32 @@ app.get('/api/persons',(req,res) =>{
     res.json(persons)
 })
 
-app.post('/api/persons',(req,res)=>{
-    
+app.get('/info',(req,res)=>{
+    res.send(`<p>Phone Book has info for ${persons.length} people <br/> ${Date()}</p>`)
 })
+
+
+// const Idgenerator = () =>{
+//     const maxId = persons.length> 0 ? Math.max(...persons.map(p => p.id)) : 0
+//     return String(maxId + 1)
+// }
+
+// app.post('/api/persons',(req,res)=>{
+//     const body = req.body   
+//     if(!body.name){
+//         return res.status(400).end(JSON.stringify({
+//             error : " Name is missing"
+//         }))
+//     }
+//     const person  = {
+//         name : body.name,
+//         number: body.number,
+//         id : Idgenerator()
+//     }
+//     persons = persons.concat(person)
+//     console.log(person)
+//     res.json(person)
+// })
 
 
 
